c32339a48f2127046e9c359ef2a93974a5cec336
Hamzaach <hamza.rotf@gmail.com>
2024-03-25 18:51:21 +0100

    partials/stylingEJS

--- index.ts
+++ index.ts
@@ -4,23 +4,45 @@ import path from "path";
 
 dotenv.config();
 
-const app : Express = express();
+const app: Express = express();
+const pages = ["players", "leagues", "settings", "logout"];
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.set('views', path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-    res.render("index", {
-        title: "Hello World",
-        message: "Hello World"
-    })
+  res.render("index.ejs");
 });
 
+app.get("/players", (req, res) => {
+  res.render("index", {
+    title: "Hello World",
+    message: "Hello World",
+    currentPage: "players", 
+  });
+});
+
+app.get("/leagues",(req,res)=>{
+  res.render("index",{
+    title: "Hello World",
+    message: "Hello World",
+    currentPage: "leagues", 
+  })
+})
+
+app.get("/settings",(req,res)=>{
+  res.render("index",{
+    title: "Hello World",
+    message: "Hello World",
+    currentPage: "settings", 
+  })
+})
+
 app.listen(app.get("port"), () => {
-    console.log("Server started on http://localhost:" + app.get('port'));
+  console.log("Server started on http://localhost:" + app.get("port"));
 });
